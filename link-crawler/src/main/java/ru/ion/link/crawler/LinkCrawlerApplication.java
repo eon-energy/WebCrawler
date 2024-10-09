c242995ed71ef3f19ae6a30072efdc58cf6d4c0d
ion <ion2005@mail.ru>
2024-10-09 17:36:06 +0300

    init

---
+++ link-crawler/src/main/java/ru/ion/link/crawler/LinkCrawlerApplication.java
@@ -0,0 +1,13 @@
+package ru.ion.link.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkCrawlerApplication.class, args);
+	}
+
+}
---
+++ link-crawler/src/test/java/ru/ion/link/crawler/LinkCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ion.link.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
